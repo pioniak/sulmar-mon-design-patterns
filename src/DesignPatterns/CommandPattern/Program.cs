c140aa10ace2b2013e4cd46bb7636b9cfd90c0c8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-20 16:04:23 +0200

    Command

--- src/DesignPatterns/CommandPattern/Program.cs
+++ src/DesignPatterns/CommandPattern/Program.cs
@@ -1,24 +1,156 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CommandPattern
 {
+
+    public class CommandTests
+    {
+        public static void Test()
+        {
+            //ICommand command1 = new PrintCommand("Hello World!", 5);
+            //command1.Execute();
+
+
+            //ICommand command2 = new ScanCommand();
+            //command2.Execute();
+
+            Queue<ICommand> commands = new Queue<ICommand>();
+            commands.Enqueue(new PrintCommand("Hello World!", 5));
+            commands.Enqueue(new PrintCommand("Hello World!", 5));
+            commands.Enqueue(new ScanCommand());
+
+
+           while(commands.Count>0)
+           {
+                ICommand command = commands.Peek();
+
+                if (command.CanExecute())
+                {
+                    commands.Dequeue().Execute();
+                }
+           }
+
+
+        }
+    }
+
+    public interface ICommand
+    {
+        void Execute();
+
+        bool CanExecute();
+    }
+
+    public class PrintCommand : ICommand
+    {
+        private readonly string content;
+        private int pages;
+
+        public PrintCommand(string content, int pages)
+        {
+            this.content = content;
+            this.pages = pages;
+        }
+
+        public bool CanExecute()
+        {
+            return pages > 0;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine($"Printing {content}...");
+
+            pages--;
+        }
+    }
+
+    public class ScanCommand : ICommand
+    {
+        public bool CanExecute()
+        {
+            return true;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Scanning...");
+        }
+
+
+    }
+
+
+    public class Printer
+    {
+        private bool hasPaper => pages > 0;
+
+        private int pages;
+
+
+        public Printer()
+        {
+            pages = 5;
+        }
+
+        public void Print(string content)
+        {
+            Console.WriteLine($"Printing {content}...");
+
+            pages--;
+        }
+
+        public bool CanPrint()
+        {
+            return hasPaper;
+        }
+
+        public void Scan()
+        {
+            Console.WriteLine("Scanning...");
+        }
+
+        public bool CanScan()
+        {
+            return pages > 0;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Command Pattern!");
 
-            Message message = new Message("555000123", "555888000", "Hello World!");
+            CommandTests.Test();
 
-            if (message.CanPrint())
+            Printer printer = new Printer();
+
+            for (int i = 0; i < 3; i++)
             {
-                message.Print();
+                if (printer.CanPrint())
+                {
+                    printer.Print("Hello World!");
+                }
             }
 
-            if (message.CanSend())
+
+            if (printer.CanScan())
             {
-                message.Send();
-            }    
+                printer.Scan();
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (printer.CanPrint())
+                {
+                    printer.Print("Hello World!");
+                }
+            }
+
+
         }
     }
 
