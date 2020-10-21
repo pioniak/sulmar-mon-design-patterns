a357cdf766c566bea64e269605359656b59fa70d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-21 14:27:06 +0200

    LampTests

--- src/DesignPatterns/StatePattern/Program.cs
+++ src/DesignPatterns/StatePattern/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Stateless;
+using System;
+using System.Timers;
 
 namespace StatePattern
 {
@@ -10,18 +12,36 @@ namespace StatePattern
 
             // OrderTest();
 
-            Lamp lamp = new Lamp();
+            IMessageService messageService = new ConsoleMessageService();
+
+            ProxyLamp lamp = new ProxyLamp(messageService);
+            lamp.TimeLimit = TimeSpan.Parse("13:00");
+
+            Console.WriteLine(lamp.Graph);
+
+
             Console.WriteLine(lamp.State);
 
-            lamp.PowerOn();
+            lamp.PushUp();
             Console.WriteLine(lamp.State);
 
-            lamp.PowerOff();
+            
+            Console.ReadKey();
+
+            lamp.PushDown();
             Console.WriteLine(lamp.State);
 
-            lamp.PowerOff();
+            lamp.PushUp();
             Console.WriteLine(lamp.State);
 
+            lamp.PushUp();
+            Console.WriteLine(lamp.State);
+
+            lamp.PushUp();
+            Console.WriteLine(lamp.State);
+
+            lamp.PushDown();
+            Console.WriteLine(lamp.State);
 
         }
 
@@ -105,45 +125,119 @@ namespace StatePattern
         Done
     }
 
-    public class Lamp
-    {
-        public LampState State { get; set; }
 
-        public Lamp()
+    public interface IMessageService
+    {
+        void Send(string message);
+    }
+
+    public class ConsoleMessageService : IMessageService
+    {
+        public void Send(string message)
         {
-            State = LampState.Off;
+            Console.WriteLine(message);
+        }
+    }
+
+    // Przykład użycia wzorca Proxy
+    public class ProxyLamp : Lamp
+    {
+        private StateMachine<LampState, LampTrigger> machine;
+
+        public string Graph => Stateless.Graph.UmlDotGraph.Format(machine.GetInfo());
+
+        public override LampState State => machine.State;
+
+        private Timer timer = new Timer(TimeSpan.FromSeconds(5).TotalMilliseconds);
+
+        public ProxyLamp(IMessageService messageService)
+            : base(messageService)
+        {
+            machine = new StateMachine<LampState, LampTrigger>(LampState.Off);
+
+            timer.Elapsed += (s, a) => machine.Fire(LampTrigger.ElapsedTime);
+
+            machine.Configure(LampState.Off)
+                // .Permit(LampTrigger.PushUp, LampState.On)
+                .PermitIf(LampTrigger.PushUp, LampState.On, () => DateTime.Now.TimeOfDay > TimeLimit, $"Limit > {TimeLimit}")
+                .IgnoreIf(LampTrigger.PushUp, () => DateTime.Now.TimeOfDay <= TimeLimit, $"Limit <= {TimeLimit}")
+                .Ignore(LampTrigger.PushDown)
+                .OnEntry(() => messageService.Send("Dziękujemy za wyłączenie światła"), nameof(messageService.Send));
+
+            machine.Configure(LampState.On)
+                .OnEntry(() => messageService.Send("Pamiętaj o wyłączeniu światła!"), nameof(messageService.Send))
+                .OnEntry(() => timer.Start(), "Start timer")
+                .Permit(LampTrigger.PushDown, LampState.Off)
+                .Permit(LampTrigger.PushUp, LampState.Red)
+                .Permit(LampTrigger.ElapsedTime, LampState.Off)
+                .OnExit(() => timer.Stop());
+
+
+            machine.Configure(LampState.Red)
+                .Permit(LampTrigger.PushUp, LampState.On)
+                .Permit(LampTrigger.PushDown, LampState.Off);
         }
 
-        public void PowerOn()
+        public override void PushUp() => machine.Fire(LampTrigger.PushUp);
+        public override void PushDown() => machine.Fire(LampTrigger.PushDown);
+
+    }
+
+
+    // dotnet add package stateless
+
+    public class Lamp
+    {
+        public virtual LampState State { get; set; }
+
+        private readonly IMessageService messageService;
+
+        public TimeSpan TimeLimit { get; set; } = TimeSpan.Parse("13:00");
+
+        public Lamp(IMessageService messageService)
         {
-            if (State == LampState.Off)
+            this.messageService = messageService;
+        }
+
+        public virtual void PushUp()
+        {
+            if (State== LampState.Off)
             {
                 State = LampState.On;
             }
             else
-                throw new InvalidOperationException($"state {State}");
-
+            if (State == LampState.On)
+            {
+                State = LampState.Red;
+            }
         }
 
-        public void PowerOff()
+        public virtual void PushDown()
         {
             if (State == LampState.On)
             {
                 State = LampState.Off;
             }
             else
-                throw new InvalidOperationException($"state {State}");
-
+           if (State == LampState.Red)
+            {
+                State = LampState.Off;
+            }
         }
+    }
 
-
-
+    public enum LampTrigger
+    {
+        PushUp,
+        PushDown,
+        ElapsedTime
     }
 
     public enum LampState
     {
         On,
-        Off
+        Off,
+        Red
     }
 
     #endregion
---
+++ src/DesignPatterns/StatePatternUnitTests/LampTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StatePatternUnitTests
+{
+    public class LampTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
