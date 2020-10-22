83107e813e9f3586bb4237ee332e0bf14a9fffb1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-22 15:01:03 +0200

    Mediator

---
+++ src/DesignPatterns/MediatorPattern/Handlers/AddDbCustomerHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatorPattern.Events;
+using MediatorPattern.IServices;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatorPattern.Handlers
+{
+    public class AddDbCustomerHandler : INotificationHandler<AddCustomerEvent>
+    {
+        private readonly ICustomerRepository customerRepository;
+
+        public AddDbCustomerHandler(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public async Task Handle(AddCustomerEvent @event, CancellationToken cancellationToken)
+        {
+            // return Task.Run(()=>customerRepository.Add(@event.Customer));
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            customerRepository.Add(@event.Customer);
+
+           // throw new Exception();
+
+            // return Task.CompletedTask;
+
+        }
+    }
+}
---
+++ src/DesignPatterns/MediatorPattern/Requests/GetCustomerRequest.cs
@@ -0,0 +1,20 @@
+﻿using MediatorPattern.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediatorPattern.Requests
+{
+    public class GetCustomerRequest : IRequest<Customer>
+    {
+        public int Id { get; private set; }
+
+        public GetCustomerRequest(int id)
+        {
+            Id = id;
+        }
+
+    }
+}
